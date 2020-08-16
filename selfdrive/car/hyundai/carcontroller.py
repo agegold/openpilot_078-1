b4410d3ca4823428e42270341ccace79f687d933
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-08-16 22:07:30 +0900

    dd

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -1,4 +1,4 @@
-from cereal import car
+from cereal import car, log
 from common.realtime import DT_CTRL
 from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.hyundai.hyundaican import create_lkas11, create_clu11, create_lfa_mfa, create_mdps12
