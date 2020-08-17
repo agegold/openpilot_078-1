d50cfdfba3dc750bee0562a1226f449d4f02669c
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-08-17 17:28:37 +0900

    dd

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -120,7 +120,7 @@ class CarController():
     UP  = interp( v_ego, self.ksBPV, self.steerdUP )
     DN  = interp( v_ego, self.ksBPV, self.steerdDN )
 
-    str_log1 = 'ego={:.0f},{},{},{},{}'.format(v_ego, self.ksBPV, MAX, UP, DN )
+    str_log1 = 'ego={:.1f} / {} /  {} / {} / {}'.format(v_ego, nPos, MAX, UP, DN )
     trace1.printf2( '{}'.format( str_log1 ) )      
     return MAX, UP, DN
   
